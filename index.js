0a4c83be7cb5ce5d1eb9a3ae9de4bca088ed1bb6
janredkarl <2122766-janredkarl@users.noreply.replit.com>
2023-08-19 12:16:26 +0000

    added images

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ let myhttp = require("http");
 let myserver = myhttp.createServer(
   function(myRequest, myResponse) {
     console.log(myRequest.url);
+    let display;
+    if (myRequest.url === "/cat") {
+      display = '<!DOCTYPE html><html><head><title>Cat</title></head><body><img src="img/cat.png" alt="a cute cat"></body></html>';
+    } else if (myRequest.url === "/dog") {
+      display = '<!DOCTYPE html><html><head><title>Cat</title></head><body><img src="img/dog.png" alt="a cute dog"></body></html>';
+    } else {
+      display = "<h2>WHAT ARE YOU DOING?!</h2>";
+    }
 
-    myResponse.writeHead( 200, { "Content-Type": "text/plain" } );
-    myResponse.end("Hello World!");
+    myResponse.writeHead(200, { "Content-Type": "text/html" });
+    myResponse.end(display);
   }
 );
 
